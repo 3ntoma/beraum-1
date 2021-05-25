800e0c08222fb3f02014dd506575b60ae9068dad
3ntoma <wazzyjim31@yandex.ru>
2021-05-25 12:28:04 +0300

    catalog scroll

--- js/customjs.js
+++ js/customjs.js
@@ -303,3 +303,28 @@ let simpleValidation = function(formID, captchaID){
       });
     });
   };
+
+/*------------------ Прокрутка категории ---------------*/
+$(function(){
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 100) {
+			$('#scroll_top').show();
+		} else {
+			$('#scroll_top').hide();
+		}
+	});
+	$('#scroll_top').click(function(){
+		$('html, body').animate({scrollTop: 0}, 600);
+		return false;
+	});
+});
+$('[href^="#catalog_"]').on('click', function(){
+	let href = $(this).attr('href'), elem = $(document).find(href);
+	if(elem.length > 0) {
+	  let posY = elem.eq(0).offset().top;
+	  $('html, body').animate({
+		scrollTop: posY
+	  }, 1000);
+	}
+	return false;
+  });
